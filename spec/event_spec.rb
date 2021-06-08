8922718a7b530c8d6be1a481eaa6fd90d1fa7ab1
Matt Kragen <mattdkragen@gmail.com>
2021-06-08 12:29:03 -0700

    Add attribute test for Event

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -11,5 +11,14 @@ RSpec.describe Event do
       event = Event.new("Carla's Craft Connection", [craft], [person])
       expect(event).to be_a(Event)
     end
+
+    it 'has attributes' do
+      craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      event = Event.new("Carla's Craft Connection", [craft], [person])
+      expect(event.name).to eq("Carla's Craft Connection")
+      expect(event.crafts).to eq([craft])
+      expect(event.attendees).to eq([person])
+    end
   end
 end
