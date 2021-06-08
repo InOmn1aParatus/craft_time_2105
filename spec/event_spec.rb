34cbc25367ec68b40084d57edae2f6919a1a9b60
Matt Kragen <mattdkragen@gmail.com>
2021-06-08 12:26:07 -0700

    Update spec test with necessary instances

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -6,6 +6,8 @@ require './lib/event'
 RSpec.describe Event do
   describe 'instantiation' do
     it 'exists' do
+      craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
       event = Event.new("Carla's Craft Connection", [craft], [person])
       expect(event).to be_a(Event)
     end
