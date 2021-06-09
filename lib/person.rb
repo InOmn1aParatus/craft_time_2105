131fb6f6d8302a0cc9776db06856c1a130502463
Matt Kragen <mattdkragen@gmail.com>
2021-06-08 18:37:07 -0700

    Add helper methods

--- lib/person.rb
+++ lib/person.rb
@@ -4,17 +4,26 @@ class Person
   def initialize(info)
     @name = info[:name]
     @interests = info[:interests]
-    @supplies = {}
+    @supplies = Hash.new(0)
   end
 
   def add_supply(supply, quantity)
-    @supplies[supply] = quantity
+    @supplies[supply] += quantity
+  end
+
+  def supplies_to_symbols
+    @supplies.keys.map do |key|
+      key.to_sym
+    end.sort
+  end
+
+  def value_comparison(craft)
+    craft.supplies_required.values.
   end
 
   def can_build?(craft)
-    tools = []
-    quantity = []
-    craft.supplies_required.each do |tool, quantity|
+    tools = craft.supplies_required.keys.sort
+    (supplies_to_symbols == tools) && 
     end
   end
 end
