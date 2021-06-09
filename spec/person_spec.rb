d174fcd903012381b766b3c56ec2a2a767b2ea6c
Matt Kragen <mattdkragen@gmail.com>
2021-06-08 18:36:46 -0700

    Create new tests for helper methods

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -28,9 +28,21 @@ RSpec.describe Person do
       expect(@person.supplies).to eq({"fabric"=>4, "scissors"=>1})
     end
 
-    it 'determines if person can craft' do
+    it 'converts supply keys to symbols' do
+      @person.add_supply('fabric', 4)
+      @person.add_supply('scissors', 1)
+      expect(@person.supplies.keys).to eq([:fabric, :scissors])
+    end
+
+    it 'compares supply value to craft requirement value' do
       sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
-      require 'pry'; binding.pry
+      @person.add_supply('fabric', 4)
+      @person.add_supply('scissors', 1)
+
+    end
+
+    xit 'determines if person can craft' do
+      sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
       expect(@hector.can_build?(sewing)).to eq(false)
       
       @hector.add_supply('fabric', 7)
